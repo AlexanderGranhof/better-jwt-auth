0f500c96712c8c1fa93622f801d66f9d5a167263
Alexander Granhof <alexander.g737@gmail.com>
2020-11-08 21:31:47 +0100

    added return types to methods

--- server/src/services/jwt.ts
+++ server/src/services/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { ACCESS_SECRET, ACCESS_EXPIRE, REFRESH_SECRET, REFRESH_EXPIRE } from '../config/env'
 
-export const createAccess = (payload: Record<string, unknown>) => {
+export const createAccess = (payload: Record<string, unknown>): Promise<string> => {
     return new Promise((resolve, reject) =>
         jwt.sign(payload, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE }, (err, token) => {
             return err ? reject(err) : resolve(token)
@@ -9,7 +9,7 @@ export const createAccess = (payload: Record<string, unknown>) => {
     )
 }
 
-export const createRefresh = (payload: Record<string, unknown>) => {
+export const createRefresh = (payload: Record<string, unknown>): Promise<string> => {
     return new Promise((resolve, reject) =>
         jwt.sign(payload, REFRESH_SECRET, { expiresIn: REFRESH_EXPIRE }, (err, token) => {
             return err ? reject(err) : resolve(token)
@@ -17,18 +17,18 @@ export const createRefresh = (payload: Record<string, unknown>) => {
     )
 }
 
-export const verifyAccess = (token: string) => {
+export const verifyAccess = (token: string): Promise<Record<string, unknown>> => {
     return new Promise((resolve, reject) =>
         jwt.verify(token, ACCESS_SECRET, (err, decoded) => {
-            return err ? reject(err) : resolve(decoded)
+            return err ? reject(err) : resolve(decoded as Promise<Record<string, unknown>>)
         }),
     )
 }
 
-export const verifyRefresh = (token: string) => {
+export const verifyRefresh = (token: string): Promise<Record<string, unknown>> => {
     return new Promise((resolve, reject) =>
         jwt.verify(token, REFRESH_SECRET, (err, decoded) => {
-            return err ? reject(err) : resolve(decoded)
+            return err ? reject(err) : resolve(decoded as Record<string, unknown>)
         }),
     )
 }
